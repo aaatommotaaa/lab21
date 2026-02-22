177eda281ea695d5c515e94c5dfbb23e04807c9e
aaatommotaaa <chanidapa.atom@gmail.com>
2026-02-22 14:48:47 +0700

    average.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char *argv[]){
+    int sum=0;
+    if (argc>1){
+        for (int i=0; i<argc; i++) {
+            sum += atof(argv[i]);
+        }
+        int count = argc-1;
+        double average = sum/count;
+        cout << "---------------------------------\n";
+        cout << "Average of " << count << " numbers = " << average << endl;
+        cout << "---------------------------------\n";
+    } else {
+        cout << "Please input numbers to find average.";
+    }
+
+}
